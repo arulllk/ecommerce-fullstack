d6245fcb9521aec84d24bead387c389638b426c9
Arul <arul.anthony@digivationworld.com>
2025-06-18 23:55:51 +0530

    added

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['m.media-amazon.com'], // 👈 add external domain here
+  },
 };
 
 export default nextConfig;
--- frontend/src/app/products/page.tsx
+++ frontend/src/app/products/page.tsx
@@ -21,17 +21,20 @@ export default async function ProductList () {
             <div className="max-w-7xl mx-auto">
                 <SortFilterBar className="mt-12 mb-7" />
 
-                <div className="mb-30">
+                <div className="mb-30 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {data.data.products.map(({_id,image, title, mrp, sellingPrice, category}:any)=>{
-                        <ProductTile
-                            image={image}
-                            name={title}
-                            price={sellingPrice}
-                            originalPrice={mrp}
-                            discount="20%"
-                            label="SALE"
-                            rating={4}                       
-                        />
+                        return(
+                            <ProductTile
+                                key={_id}
+                                image={image}
+                                name={title}
+                                price={sellingPrice}
+                                originalPrice={mrp}
+                                discount="20%"
+                                label="SALE"
+                                rating={4}                       
+                            />
+                        )
                     })}
                 </div>
             </div>
